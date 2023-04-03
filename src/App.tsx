7abbb41cf8fc96a5fea89c3d5552ea98d7de3c81
Aish1007 <aishwarya.narayanan025@gmail.com>
2023-04-02 18:06:27 -0700

    Add input field and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-[#283040]">
+      <div className="App flex  justify-center p-10">
+      <input className="border border-1 border-black mx-10 w-64" /> 
+      <button
+        className="px-10 py-2 bg-blue-900 border-2 border-blue-900 text-white rounded-sm"
+      >
+        View results
+      </button>
+    </div>
     </div>
   );
 }
