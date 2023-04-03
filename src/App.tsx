a28505db9f0d1095060ec3849ffe83d6ec387b84
Aish1007 <aishwarya.narayanan025@gmail.com>
2023-04-02 18:46:48 -0700

    Set match results

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import  { AxiosResponse } from 'axios';
+import { useState } from 'react';
 import './App.css';
+import { Matches } from './types/Match';
+import axios from "./request/index"
 
 function App() {
+  const [summonerName, setSummonerName] = useState('');
+  const [matchHistory, setMatchHistory] = useState<Matches>();
+
+
+  const getMatchResults = () => {
+       axios.get(summonerName).then((res: AxiosResponse)=> {
+        setMatchHistory(res.data)
+       })
+  }
+
+
   return (
     <div className="App bg-[#283040]">
       <div className="App flex  justify-center p-10">
-      <input className="border border-1 border-black mx-10 w-64" /> 
+      <input className="border border-1 border-black mx-10 w-64" value={summonerName} onChange={(e) => setSummonerName(e.target.value)} /> 
       <button
         className="px-10 py-2 bg-blue-900 border-2 border-blue-900 text-white rounded-sm"
+        onClick={getMatchResults}
       >
         View results
       </button>
